syberia-project/platform_packages_apps_Settings

SettingsGateway: publish for ambient play tile
Change-Id: I46fc34f228d9577d31dd4a21520f9226e8208172
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -141,6 +141,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.syberia.settings.SyberiaSettings;
 import com.syberia.settings.fragments.OmniJawsSettings;
 import com.android.settings.custom.ambient.history.AmbientPlayHistory;
+import com.android.settings.custom.ambient.AmbientPlaySettings;
 
 public class SettingsGateway {
 
@@ -270,6 +271,7 @@ public class SettingsGateway {
             SyberiaSettings.class.getName(),
             OmniJawsSettings.class.getName(),
             AmbientPlayHistory.class.getName(),
+            AmbientPlaySettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

sprinix/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -145,6 +145,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.stag.horns.Horns;
 import com.stag.horns.fragments.HeadsUpSettings;
 import com.android.settings.custom.ambient.history.AmbientPlayHistory;
+import com.android.settings.custom.ambient.AmbientPlaySettings;
 
 public class SettingsGateway {
 
@@ -277,7 +278,8 @@ public class SettingsGateway {
             SmartbarSettingsActivity.class.getName(),
             FlingSettingsActivity.class.getName(),
             PulseSettingsActivity.class.getName(),
-            AmbientPlayHistory.class.getName()
+            AmbientPlayHistory.class.getName(),
+            AmbientPlaySettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {